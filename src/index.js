3b8aef5446bd885094ca5ab65ecb3310beb04d4b
aliaksandrow <aliaksandrou@gmail.com>
2020-10-01 14:06:29 +0300

    docs: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
 exports.min = function min (array) {
   if(!array) return 0;
-   return array.reduce((acc,i) => Math.min(acc,i), 0)
+   return array.reduce((acc,i) => Math.min(acc,i), 0);
 }
 
 exports.max = function max (array) {
   if(!array) return 0;
-  return array.reduce((acc,i) => Math.max(acc,i), 0)
+  return array.reduce((acc,i) => Math.max(acc,i), 0);
 }
 
 exports.avg = function avg (array) {
   if(!array) return 0;
-  let sum = array.reduce((acc,i) => acc + i, 0)
+  let sum = array.reduce((acc,i) => acc + i, 0);
   return sum / array.length || 0;
 }
